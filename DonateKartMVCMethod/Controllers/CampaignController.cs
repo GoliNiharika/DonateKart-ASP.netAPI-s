000a546e679a523227579d52094dbb2624e8d8e6
Niharika Goli <niharika25g@gmail.com>
2021-07-19 10:22:10 +0530

    API calls in MVc Method in ASP .net

---
+++ DonateKartMVCMethod/Controllers/CampaignController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using DonateKartMVCMethod.Models;
+
+namespace DonateKartMVCMethod.Controllers
+{
+    public class CampaignController : Controller
+    {
+        // GET: Campaign
+        public ActionResult Index()
+        {
+            IEnumerable<CampaignModel> campaigns = null;
+            using(var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://testapi.donatekart.com/api/");
+                var responseTask = client.GetAsync("campaign");
+                responseTask.Wait();
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var read = result.Content.ReadAsAsync<IList<CampaignModel>>();
+                    read.Wait();
+                    campaigns = read.Result;
+                }
+                else
+                {
+                    campaigns = Enumerable.Empty<CampaignModel>();
+                    ModelState.AddModelError(string.Empty, "Error occured");
+                }
+            }
+            return View(campaigns);
+        }
+    }
+}
---
+++ DonateKartMVCMethod/Models/CampaignModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DonateKartMVCMethod.Models
+{
+    public class CampaignModel
+    {
+        public string code { get; set; }
+        public string title { get; set; }
+        public bool featured { get; set; }
+        public int priority { get; set; }
+        public string shortDesc { get; set; }
+        public string imageSrc { get; set; }
+        public DateTime created { get; set; }
+        public DateTime endDate { get; set; }
+        public float totalAmount { get; set; }
+        public float procuredAmount { get; set; }
+        public float totalProcured { get; set; }
+        public float backersCount { get; set; }
+        public int categoryId { get; set; }
+        public string ngoCode { get; set; }
+        public string ngoName { get; set; }
+        public int daysLeft { get; set; }
+        public float percentage { get; set; }
+    }
+}
